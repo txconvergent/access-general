16934834fd7ac7579e9a433cfd7c3a9753cf3b00
Ana Brendel <anabrendel@wireless-10-145-252-11.public.utexas.edu>
2019-04-09 18:37:39 -0500

    Adding in camera code - from site

--- script.js
+++ script.js
@@ -10,4 +10,15 @@ var dict = {A: "imageRef",
     D: "imageRef"  };
 
 
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    })
+};
 </script>
