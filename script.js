d5e8026b04c1c965918f56050c6fa876a4ab502c
Ana Brendel <anabrendel@wireless-10-145-252-11.public.utexas.edu>
2019-04-09 18:15:36 -0500

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+function chooseJPG(letter){
+
+    
+}
+    
+var dict = {A: "imageRef",
+    B: "imageRef",
+    C: "imageRef",
+    D: "imageRef"  };
+
+
+</script>
